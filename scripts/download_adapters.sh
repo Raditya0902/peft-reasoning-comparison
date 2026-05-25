aa2b40508d1ef2acf5759a2482e02bb7dcbc6b0f
Raditya0902 <aditya.rallapalli0902@gmail.com>
2026-05-24 22:35:05 -0700

    fix download_adapters.sh, remove stale local training log

---
+++ scripts/download_adapters.sh
@@ -0,0 +1,17 @@
+PROJECT=dllm-refactor-2026
+ZONE=us-central1-a
+INSTANCE=peft-training
+
+gcloud compute scp --recurse \
+  adityarallapalli7@${INSTANCE}:/home/adityarallapalli7/peft-reasoning-comparison/adapters/lora \
+  ./adapters/ --zone=${ZONE} --project=${PROJECT}
+
+gcloud compute scp --recurse \
+  adityarallapalli7@${INSTANCE}:/home/adityarallapalli7/peft-reasoning-comparison/adapters/dora \
+  ./adapters/ --zone=${ZONE} --project=${PROJECT}
+
+gcloud compute scp --recurse \
+  adityarallapalli7@${INSTANCE}:/home/adityarallapalli7/peft-reasoning-comparison/adapters/ia3 \
+  ./adapters/ --zone=${ZONE} --project=${PROJECT}
+
+echo "All adapters downloaded."
